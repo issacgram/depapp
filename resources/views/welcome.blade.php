4f648d15d7ffbb6d087cf3e9ee858e4dee46435e
issacgram <issac.php@gmail.com>
2024-12-09 20:24:21 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>TEst 2</title>
+        <title>From fithub</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
