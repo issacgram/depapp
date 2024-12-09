6ddc0d021868574ad0dc5f1204054cc86b078198
issacgram <isn@iass.com>
2024-12-09 18:33:30 +0800

    Zeba 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>TEst 1</title>
+        <title>TEst 2</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
